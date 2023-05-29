f097080bfa9349c6098d71b98e24431f7be73811
Nkaleth <nseguralu@gmail.com>
2023-05-29 17:08:27 -0500

    FIX typo reference to references

--- db/migrate/20230529204428_create_payments.rb
+++ db/migrate/20230529204428_create_payments.rb
@@ -1,7 +1,7 @@
 class CreatePayments < ActiveRecord::Migration[7.0]
   def change
     create_table :payments do |t|
-      t.reference :author_id, null: false, foreign_key: { to_table: :users }, index: true
+      t.references :author_id, null: false, foreign_key: { to_table: :users }, index: true
       t.string :name, null: false
       t.float :amount, null: false
       t.timestamps
