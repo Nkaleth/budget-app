060e12276ae6573b97d74c30a9c0d6d3b5332b90
Nkaleth <nseguralu@gmail.com>
2023-05-29 17:14:39 -0500

    FIX foreign key columns names from categories and payments on migrate files

--- db/migrate/20230529204327_create_categories.rb
+++ db/migrate/20230529204327_create_categories.rb
@@ -3,7 +3,7 @@ class CreateCategories < ActiveRecord::Migration[7.0]
     create_table :categories do |t|
       t.string :name, null: false
       t.string :icon, null: false
-      t.references :author_id, null: false, foreign_key: { to_table: :users }, index: true
+      t.references :author, null: false, foreign_key: { to_table: :users }, index: true
       t.timestamps
     end
   end
