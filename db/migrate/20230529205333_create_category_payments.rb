7ded1d9d4ef0cc322229b0727f8381cbd1fd6327
Nkaleth <nseguralu@gmail.com>
2023-05-29 17:07:00 -0500

    ADD colums to the categoryPayments table referencing categories and payments tables

--- db/migrate/20230529205333_create_category_payments.rb
+++ db/migrate/20230529205333_create_category_payments.rb
@@ -1,7 +1,8 @@
 class CreateCategoryPayments < ActiveRecord::Migration[7.0]
   def change
     create_table :category_payments do |t|
-
+      t.references :category, null: false, foreign_key: true
+      t.references :payment, null: false, foreign_key: true
       t.timestamps
     end
   end
