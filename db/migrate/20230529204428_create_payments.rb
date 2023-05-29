d51fcba09d7ef018af173dc7ad2f3b63841b1f47
Nkaleth <nseguralu@gmail.com>
2023-05-29 17:04:09 -0500

    ADD columns of payments table including foreign key to users

--- db/migrate/20230529204428_create_payments.rb
+++ db/migrate/20230529204428_create_payments.rb
@@ -1,7 +1,9 @@
 class CreatePayments < ActiveRecord::Migration[7.0]
   def change
     create_table :payments do |t|
-
+      t.reference :author_id, null: false, foreign_key: { to_table: :users }, index: true
+      t.string :name, null: false
+      t.float :amount, null: false
       t.timestamps
     end
   end
