580e9c0235b555b9a714774138071ec793f499f0
Nkaleth <nseguralu@gmail.com>
2023-05-29 19:19:47 -0500

    ADD relationship of user model and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :payments, foreign_key: :author_id, dependent: :destroy
+  has_many :categories, foreign_key: :author_id, dependent: :destroy
+
+  validates :name, presence: true
 end
