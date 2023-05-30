32a42f736c8188b41be7bf0c63859b3995045ee4
Nkaleth <nseguralu@gmail.com>
2023-05-30 12:28:13 -0500

    ADD seeds for users and categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+# Tom user's
+
+first_user = User.create!(name: 'Tom', user_avatar: 'https://drive.google.com/uc?id=1vDckZ740UU_vCPzcDF1Uaqy-95jQYvAE', email: 'tom@email.com', password: '123456')
+second_user = User.create!(name: 'Lilly', user_avatar: 'https://drive.google.com/uc?id=1vDckZ740UU_vCPzcDF1Uaqy-95jQYvAE', email: 'lilly@email.com', password: '123456')
+third_user = User.create!(name: 'Luis', user_avatar: 'https://drive.google.com/uc?id=1vDckZ740UU_vCPzcDF1Uaqy-95jQYvAE', email: 'luis@email.com', password: '123456')
+
+# Tom's Categories
+
+first_categ = Category.create!(name: 'Food', icon: 'https://drive.google.com/uc?id=1tvEXRtI0n2mRIyHO6jWTr01CNIFQaHER', author: first_user)
+second_categ = Category.create!(name: 'Education', icon: 'https://drive.google.com/uc?id=1Igv3yU_4o3ry6HQzsN6GXhGivGcJ3nrt', author: first_user)
+third_categ = Category.create!(name: 'Pet', icon: 'https://drive.google.com/uc?id=13FSBgZOX3uR-CKxz89Rrhu67OcawWgQ5', author: first_user)
+fourth_categ = Category.create!(name: 'Health', icon: 'https://drive.google.com/uc?id=1fwwSWOnnOMKvypxTeqWw6pbl-S7SP4J0', author: first_user)
+
+# Lilly Categories
+
+sixth_categ = Category.create!(name: 'Entertainment', icon: 'https://drive.google.com/uc?id=1hDAfXduhiyHl1Wa3jJMXoO5pAm2y25N2', author: second_user)
