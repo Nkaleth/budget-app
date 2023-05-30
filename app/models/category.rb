0bab0ffaa9bb0ba367121ecd35ef9b127d3435e3
Nkaleth <nseguralu@gmail.com>
2023-05-29 21:13:30 -0500

    ADD relationships for category model and validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,8 @@
 class Category < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :category_payments
+  has_many :payments, through: :category_payments
+
+  validates :name, presence: true
+  validates :icon, presence: true
 end
