6f498d0c5e0a9d44893c568c6e072fffac2c5800
Nkaleth <nseguralu@gmail.com>
2023-05-30 10:53:53 -0500

    ADD basic abilities fo author's role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :read, Category, Payment, user_id: user.id if user.author?
+    can :manage, Category, Payment, user_id: user.id if user.author?
 
     return unless user.admin? # additional permissions for administrators
 
