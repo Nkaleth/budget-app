9a3d91217651ab496b54d4ce987104f8908bcfc1
Nkaleth <nseguralu@gmail.com>
2023-06-02 15:59:57 -0500

    ADD integration test for categories index page

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,4 +60,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include Warden::Test::Helpers
+
+  config.after :each do
+    Warden.test_reset!
+  end
 end
